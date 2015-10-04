cc3eef001e512f780a8cc0cae69e5649e85f4f7c
Carl Li <huabeiyipilang@gmail.com>
2015-10-04 16:27:46 +0800

    Initial Commit

---
+++ clframework/clframework.h
@@ -0,0 +1,13 @@
+//
+//  clframework.h
+//  clframework
+//
+//  Created by Carl Li on 10/4/15.
+//  Copyright (c) 2015 Carl Li. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface clframework : NSObject
+
+@end
---
+++ clframework/clframework.m
@@ -0,0 +1,13 @@
+//
+//  clframework.m
+//  clframework
+//
+//  Created by Carl Li on 10/4/15.
+//  Copyright (c) 2015 Carl Li. All rights reserved.
+//
+
+#import "clframework.h"
+
+@implementation clframework
+
+@end
